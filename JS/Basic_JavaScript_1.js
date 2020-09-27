b90dc1b99b8e6eebb80e8f5ede130c008261c838
Foysal91 <foysallawdubd@gmail.com>
2020-09-27 06:24:11 +0100

    first commit

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,42 @@
+//Please uncomment individually to see the result
+
+//Setting an alert
+//window.alert('Hello World');
+
+//Writing on DOM
+//document.write('Hello world');
+
+// Assignig string value to a var
+// var str = 'This is a string';
+// document.write(str);
+
+//Assigning a string value to variable
+// var strValue = 'This is an alert';
+// window.alert(strValue);
+
+//A string containing apostropher and quotations marks
+//document.write('As John said, "He likes to code", so I shouldn\'t discourage him.')
+
+//Concatenate string 
+// document.write("I care what I do, " 
+// + "So I do it sincerely " 
+// + " to have the best possible outcome.");
+
+// Assigning mulpile variables in one statement
+var language = 'countries',
+    french = 'Franch',
+    german = 'Germany';
+//document.write(german);
+
+//Assigning colors to all the variables
+var language = language.fontcolor('red');
+var french = french.fontcolor('green');
+var german = german.fontcolor('yellow');
+//document.write(`${language} ${french} ${german}`);
+
+//Expression
+29 + 9; //Execution is not possible without an statment
+
+
+
+
---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,15 @@
+//Assigning two variables in a function
+function assignTwoVar(){
+    var str = 'This is a string';
+    var result = str.fontcolor('red');
+    document.getElementById('assignTwoVar').innerHTML = result;
+};
+
+//Concatenate a string
+function utilizeId(){
+    var str = 'This is a string';
+        str += ' with a concanation.';
+        document.getElementById('utilizeId').innerHTML = str;
+}
+
+
